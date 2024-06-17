cda53db35e7241c116d7b94316e1129e5f370879
fabio ragacini <fabioragacini@icloud.com>
2024-06-17 14:38:34 -0300

    first commit

---
+++ src/main/java/br/com/ragacini/oimundo/OiMundo.java
@@ -0,0 +1,13 @@
+
+package br.com.ragacini.oimundo;
+
+/**
+ *
+ * @author Aluno
+ */
+public class OiMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
